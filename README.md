9a851edaa7911ef300e70d383b2a78bcc5bc759d
Zane Williamson <zwilliamson@machinezone.com>
2014-09-22 23:16:58 -0700

    adding checks for generate gpg, adding second test server to apt-get install from aplty server

--- README.md
+++ README.md
@@ -6,10 +6,17 @@ Vagrant provision will setup Aptly repos and publish them to aptly.localhost:808
 
 Nginx is the frontend.  Accessible via aptly.localhost:8080
 
-Add this line to your application's Gemfile:
-
+'vagrant up' will bring up 2 virutal servers.  One is called reposerver and runs your test Aptly repo.  The other is server1 which can install debs from the Aptly repo VM.
     vagrant up 
-    vagrant ssh
+
+Vagrant ssh to reposerver
+    vagrant ssh reposerver
+
+Vagrant ssh to client server
+    vagrant ssh server1
+
+Use 'reposerver' to test your aptly commands and configurations
+Use 'server1' to do your apt-get install
 
 Test out Aptly apt repository commands, eg
 
--- generate_gpg.sh
+++ generate_gpg.sh
@@ -6,7 +6,7 @@ echo "HRNGDEVICE=/dev/urandom" > /etc/default/rng-tools
 service rng-tools restart
 
 #Setup batch file for gpg to use, for no prompt action
-cat >/vagrant_data/vagrant_key <<EOF
+cat >/vagrant/vagrant_key <<EOF
           %echo Generating a basic OpenPGP key
           Key-Type: DSA
           Key-Length: 1024 
@@ -22,10 +22,19 @@ cat >/vagrant_data/vagrant_key <<EOF
           %echo done
 EOF
 
-#Generate gpg key
-gpg --batch --gen-key /vagrant_data/vagrant_key
-gpg --import vagrant.pub
-gpg --import vagrant.sec
+if [ ! -f "/vagrant/vagrant.pub" ]; then
+  #Generate gpg key
+  gpg --batch --gen-key /vagrant/vagrant_key
+  gpg --import vagrant.pub
+  gpg --import vagrant.sec
+  
+  #Export key to Apt
+  gpg -a --export vagrant > /vagrant/vagrant.pub
+  gpg -a --export-secret-key vagrant > /vagrant/vagrant.sec
+  gpg -a --export vagrant | apt-key add -
 
-#Export key to Apt
-gpg -a --export vagrant | apt-key add -
+else 
+  gpg --import /vagrant/vagrant.pub
+  gpg --allow-secret-key-import /vagrant/vagrant.sec
+fi
+
