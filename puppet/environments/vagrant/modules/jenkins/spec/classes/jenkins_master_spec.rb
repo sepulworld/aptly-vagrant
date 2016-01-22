3d0acab13ce81e32635532914513f713f6bb4321
Zane <zane.williamson@gmail.com>
2016-01-21 22:13:26 -0800

    using r10k for puppet module dep management

--- puppet/environments/vagrant/modules/jenkins/spec/classes/jenkins_master_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'jenkins::master' do
-  let(:facts) {{ :osfamily => 'RedHat', :operatingsystem => 'CentOS' }}
-
-  let(:params) { { :version => '1.2.3' } }
-  it { should contain_jenkins__plugin('swarm').with_version('1.2.3') }
-
-end
--- puppet/environments/vagrant/modules/sudo/lib/facter/sudoversion.rb
+++
@@ -1,8 +0,0 @@
-require 'puppet'
-Facter.add("sudoversion") do
-  setcode do
-    if Facter::Util::Resolution.which('rpm')
-      sudoversion = Facter::Util::Resolution.exec('rpm -q sudo --nosignature --nodigest --qf \'%{VERSION}\'')
-    end
-  end
-end
